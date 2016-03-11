5a65ea465add689518dbdbf2c6ead9b869e0e13c
jnadeau <jnadeau@localhost.localdomain>
2016-03-11 11:54:46 -0500

    Initial commit before I beg for help and mercy from others.

---
+++ src/main/resources/local/jnadeau/xmlrest/jaxbandjersey/XMLService.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package local.jnadeau.xmlrest.jaxbandjersey;
+
+/**
+ *
+ * @author jnadeau
+ */
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+
+
+@Path("/customer")
+public class XMLService {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getEcho() {
+    
+        String echo = "ECHO!";
+        return echo;
+    
+    }
+    
+	@GET
+	@Path("/{pin}")
+	@Produces(MediaType.APPLICATION_XML)
+	public Customer getCustomerInXML(@PathParam("pin") int pin) {
+
+		Customer customer = new Customer();
+		customer.setName("mkyong");
+		customer.setPin(pin);
+
+		return customer;
+
+	}
+    
+}
